a01a43e5af77473e8dcc0a752f4b0c8c64f8388d
Hai Nguyen <hai_nguyen@mnb.uscourts.gov>
2017-12-29 23:45:18 -0600

    Adding project page

--- server.js
+++ server.js
@@ -42,6 +42,13 @@ app.get('/', (req,res) => {
   })
 })
 
+app.get('/project', (req,res) => {
+  res.render('project.hbs', {
+    pageTitle: 'Project Page',
+    profileMessage: "Welcome to My Profile Page"
+  })
+})
+
 app.get('/about', (req,res) => {
   res.render('about.hbs', {
     pageTitle: 'About Page'
